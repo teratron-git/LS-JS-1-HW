b54f4bebe1097754a32a65ba8ee55d4cb260269e
Sergei <rat87@mail.ru>
2020-02-18 21:33:48 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+/* ДЗ 6 - Асинхронность и работа с сетью */
+
+/*
+ Задание 1:
+
+ Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
+
+ Пример:
+   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
+ */
+function delayPromise(seconds) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), seconds * 1000);
+    });
+}
+
+/*
+ Задание 2:
+
+ 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов можно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+
+ 2.2: Элементы полученного массива должны быть отсортированы по имени города
+
+ Пример:
+   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
+ */
+async function loadAndSortTowns() {
+    let url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json',
+        response = await fetch(url);
+    if (response.ok) {
+        let cities = await response.json();
+
+        cities.sort(function (a, b) {
+            if (a.name > b.name) {
+                return 1;
+            }
+            if (a.name < b.name) {
+                return -1;
+            }
+
+            return 0;
+        });
+
+        return Promise.resolve(cities);
+    } else {
+        return Promise.reject('Не удалось загрузить города');
+    }
+}
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
